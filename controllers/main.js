d528e082222c22fb0304777d1988860c31891748
Toba Madamori <tobajamezmadamori@gmail.comgit config --global user.name Toba>
2022-01-25 17:47:38 +0100

    implemented the json web token

--- controllers/main.js
+++ controllers/main.js
@@ -1,4 +1,4 @@
-
+const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req,res)=>{
@@ -7,8 +7,14 @@ const login = async (req,res)=>{
     if(!username || !password){
         throw new CustomAPIError('Please provide the username and password', 400)
     }
-    console.log(username,password);
-    res.send('Fake login/signup/register route')
+    // dummy id to send back to the frontend with the jwt
+    const id = new Date().getDate()
+
+    // jwt process, try to keep the payload small:better user experience
+    // for the jwt secret use long,uncomprehensible and unguessable secret, the below is just for demo
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET,{expiresIn:'30d'})
+
+    res.status(200).json({msg:'user created', token})
 }
 
 const dashboard = async (req,res)=>{
